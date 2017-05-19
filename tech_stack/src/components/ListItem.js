33ed4770f485d9c12fc8a56ce0569405ca943416
Renan Rhoden <renanrhoden@gmail.com>
2017-05-19 15:16:22 -0300

    item 94 section 13 creating the selection reducer

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Text } from 'react-native';
+import { CardSection } from './common';
 
-class listItem extends Component {
+class ListItem extends Component {
   render() {
+    const { titleStyle } = styles;
 
+    return (
+      <CardSection>
+        <Text>
+          {this.props.library.title}
+        </Text>
+      </CardSection>
+    );
   }
-
 }
+
+const styles = {
+  titleStyle: {
+    fontSize: 18,
+    paddingLeft: 15
+  }
+};
 
 export default ListItem;
