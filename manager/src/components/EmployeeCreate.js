226916d2b94d069d74616702e52ab3daa2763ab2
Renan Rhoden <renanrhoden@gmail.com>
2017-05-24 14:46:17 -0300

    item 132 buinding the employee creation form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text> Employee Form</Text>
-      </View>
+      <Card>
+
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="9-9999-9999"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+
+
+
+
+      </Card>
     );
   }
 }
