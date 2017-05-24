dc76f24b9456187abc95d5b782c739a57c663cf9
Renan Rhoden <renanrhoden@gmail.com>
2017-05-24 14:35:39 -0300

    item 131 navigating to the employee creation form

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import { Actions } from 'react-native-router-flux';
 
 const RouterComponent = () => {
 
@@ -12,12 +14,20 @@ const RouterComponent = () => {
       </Scene>
       <Scene key="main">
         <Scene
-        onRight={() => console.log('right!!!')}
-        rightTitle="+"
+        onRight={() => Actions.employeeCreate()}
+        rightTitle="Add"
         key="employeeList"
         component={EmployeeList}
         title="Employee"
+        initial
         />
+
+        <Scene
+        key="employeeCreate"
+        component={EmployeeCreate}
+        title="Create Employee"
+        />
+
       </Scene>
     </Router>
   );
